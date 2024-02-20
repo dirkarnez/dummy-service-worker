1abe023316a631524ac5ae04beb8ebe8d4350d10
Dirk Arnez <smalldirkalex@gmail.com>
2024-02-20 21:33:17 +0800

    - add sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.min.js');
+
+const SW_VERSION = '1.0.0';
+
+self.addEventListener('message', (event) => {
+  if (event.data.type === 'GET_VERSION') {
+    event.ports[0].postMessage(SW_VERSION);
+  }
+});
+
