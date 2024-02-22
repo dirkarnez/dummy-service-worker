967c21db0c771a696f24aba15ea97d2997626c5e
Dirk Arnez <smalldirkalex@gmail.com>
2024-02-23 00:40:16 +0800

    - update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,20 @@ importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-s
 
 
 self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'EVAL') {
-    event.ports[0].postMessage();
+  // if (event.data && event.data.type === 'EVAL') {
+  //   event.ports[0].postMessage();
+  // }
+
+  if (event.data.type === 'CACHE_CACHEFIRST') {
+    workbox.routing.registerRoute(
+      new RegExp(event.data.regexPattern),
+      new workbox.strategies.CacheFirst()
+    );
+    debugger;
+    event.ports[0].postMessage("ok");
   }
 });
+
+
+
+
