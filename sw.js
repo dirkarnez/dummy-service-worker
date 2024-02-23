cf964e4a4e45c68392d6b57e4a8c8d48a473da3c
Dirk Arnez <smalldirkalex@gmail.com>
2024-02-23 22:39:01 +0800

    - update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,43 @@
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.min.js');
+
+let urlPattern = "";
 
 self.addEventListener('message', (event) => {
   // if (event.data && event.data.type === 'EVAL') {
   //   event.ports[0].postMessage();
   // }
-  debugger;
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.min.js');
   if (event.data.type === 'CACHE_CACHEFIRST') {
-    
+    pattern = event.data.regexPattern;
 
-
-    workbox.routing.registerRoute(
-      new RegExp(event.data.regexPattern),
-      new workbox.strategies.CacheFirst()
-    );
-    debugger;
+    // workbox.routing.registerRoute(
+    //   new RegExp(),
+    //   new workbox.strategies.CacheFirst()
+    // );
+    // debugger;
     event.ports[0].postMessage("ok");
   }
 });
 
+self.addEventListener('fetch', event => {
+  // Check if the requested URL matches the pattern
+  if (event.request.url.match(urlPattern)) {
+    event.respondWith(
+      caches.open('my-cache').then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetchAndCache(event.request, cache);
+        });
+      })
+    );
+  }
+});
 
 
+function fetchAndCache(request, cache) {
+  return fetch(request).then(response => {
+    if (response.status === 200) {
+      cache.put(request, response.clone());
+    }
+    return response;
+  });
+}
 
