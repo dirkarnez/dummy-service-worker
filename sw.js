00436b0cdc5256a02f6321997d65c95f65df300c
Dirk Arnez <smalldirkalex@gmail.com>
2024-02-23 22:09:42 +0800

    - update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.min.js');
-
 
 self.addEventListener('message', (event) => {
   // if (event.data && event.data.type === 'EVAL') {
   //   event.ports[0].postMessage();
   // }
   debugger;
-
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.min.js');
   if (event.data.type === 'CACHE_CACHEFIRST') {
+    
+
+
     workbox.routing.registerRoute(
       new RegExp(event.data.regexPattern),
       new workbox.strategies.CacheFirst()
